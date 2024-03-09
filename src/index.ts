3b2f101db1ef301ac3e8083ead1de13d1f9a3c36
Navin Panthi <panthinabin341@gmail.com>
2024-03-09 22:26:05 +0545

    Add folder for database configuration

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+//load environment variables from a .env file into process.env.
+if (!process.env.PORT) {
+  process.exit(1);
+}
+const PORT: number = parseInt(process.env.PORT as string, 10);
+// here 10 indicates base 10 (decimal), so the string is parsed as a decimal number.
+
+const app = express();
+app.use(cors());
+// This line adds the CORS (Cross-Origin Resource Sharing) middleware to your Express application.
+app.use(express.json());
+// parse incoming requests with JSON payloads.
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
